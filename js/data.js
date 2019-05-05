413052dbd10c2533003b8c0677a719af82d2e454
Lilian Guimarães <lilian.guimaraes.silva@gmail.com>
2019-05-05 20:38:08 -0300

    Update data.js

--- js/data.js
+++ js/data.js
@@ -3,93 +3,59 @@ const data = {
     "SP": [{ "slug": "sao-paulo", "nome": "São Paulo", "num": 40 }]
 }
 
-var selectState = document.querySelector('select[id=state');
-// populate select state option with data structure
+var selectState = document.querySelector('#state');
 selectState.options[selectState.options.length] = new Option('---', '---')
 for (var key in data) {
     selectState.options[selectState.options.length] = new Option(key, key)
 }
 
-// find municipies value inside the object
-let selectMunicipies = document.querySelector('select[id=municipie]');
-selectMunicipies.options[selectMunicipies.options.length] = new Option('---', '---')
-for (var key of Object.keys(data)) {
-    for (var city of data[key]) {
+let selectMunicipies = document.querySelector('#municipie');
+selectState.addEventListener('change', function() {
+    selectMunicipies.innerHTML = "";
+    if (selectState.value == "---") return;
+    selectMunicipies.options[selectMunicipies.options.length] = new Option('---', '---')
+    for (var city of data[selectState.value]) {
         selectMunicipies.options[selectMunicipies.options.length] = new Option(city.nome, city.slug)
     }
-}
-
-//onChange if it's isMinifundio
-document.querySelector('select[id=municipie]').addEventListener('change', function() {
-    let current = document.querySelector('select[id=fiscalModule]');
-    let isMinifundio = current.options[current.selectedIndex].value == 'minifundio' ? true : false;
-    console.log(this.value);
-    console.log('state', state);
-    var municipie = state.find(x => x.slug === this.value)
-    console.log('find', find);
-    if (isMinifundio) {
-        document.querySelector('input[id=module]').value = municipie.num;
-        document.querySelector('p[id=propertieClassification]').innerHTML = 'Minifundio';
-        document.querySelector('p[id=app_margem_corrego]').innerHTML = '5 metros';
-        document.querySelector('p[id=app_nascente]').innerHTML = '15 metros';
-        document.querySelector('p[id=reseva_legal]').innerHTML = 'A mata que já existe na propriedade';
-
-    }
 })
+document.querySelector('#fiscalModule').addEventListener('change', function() {
+    // get selected state
+    var state = undefined;
+    if (selectState.value != "---")
+        state = data[selectState.value];
+    // get selected state
+    var municipie = undefined;
+    if (state)
+        municipie = state.find(x => x.slug === selectMunicipies.value);
+    if (municipie)
+        document.querySelector('#module').innerHTML = municipie.num;
+    if (this.value == "minifundio") {
+        document.querySelector('#propertieClassification').innerHTML = 'Minifundio';
+        document.querySelector('#app_margem_corrego').innerHTML = '5 metros';
+        document.querySelector('#app_nascente').innerHTML = '15 metros';
+        document.querySelector('#reseva_legal').innerHTML = 'A mata que já existe na propriedade';
 
-//onChange if it's smallPropertie
-document.querySelector('select[id=municipie]').addEventListener('change', function() {
-    const state = data[selectState.options[selectState.selectedIndex].value]
-    let current = document.querySelector('select[id=fiscalModule]');
-    let isSmallPropertie = current.options[current.selectedIndex].value == 'smallPropertie' ? true : false;
-    var municipie = state.find(x => x.slug === this.value)
-    if (isSmallPropertie) {
-        document.querySelector('input[id=module]').value = municipie.num;
-        document.querySelector('p[id=propertieClassification]').innerHTML = 'pequena propriedade';
-        document.querySelector('p[id=app_margem_corrego]').innerHTML = '8 metros';
-        document.querySelector('p[id=app_nascente]').innerHTML = '15 metros';
-        document.querySelector('p[id=reseva_legal]').innerHTML = 'A mata que já existe na propriedade';
-    }
-})
+    } else if (this.value == "smallPropertie") {
+        document.querySelector('#propertieClassification').innerHTML = 'pequena propriedade';
+        document.querySelector('#app_margem_corrego').innerHTML = '8 metros';
+        document.querySelector('#app_nascente').innerHTML = '15 metros';
+        document.querySelector('#reseva_legal').innerHTML = 'A mata que já existe na propriedade';
 
-//onChange if it's medium and 2_mediumPropertie
-document.querySelector('select[id=municipie]').addEventListener('change', function() {
-    const state = data[selectState.options[selectState.selectedIndex].value]
-    let current = document.querySelector('select[id=fiscalModule]');
-    let isMediumPropertie = current.options[current.selectedIndex].value == 'mediumPropertie' ? true : false;
-    var municipie = state.find(x => x.slug === this.value)
-    if (isMediumPropertie) {
+    } else if (this.value == "mediumPropertie") {
         document.querySelector('input[id=module]').value = municipie.num;
         document.querySelector('p[id=propertieClassification]').innerHTML = 'Média propriedade';
         document.querySelector('p[id=app_margem_corrego]').innerHTML = '15 metros';
         document.querySelector('p[id=app_nascente]').innerHTML = '15 metros';
         document.querySelector('p[id=reseva_legal]').innerHTML = 'A mata que já existe na propriedade';
-    }
-})
 
-//onChange 2_mediumPropertie
-document.querySelector('select[id=municipie]').addEventListener('change', function() {
-    const state = data[selectState.options[selectState.selectedIndex].value]
-    let current = document.querySelector('select[id=fiscalModule]');
-    let is_2MediumPropertie = current.options[current.selectedIndex].value == '2_mediumPropertie' ? true : false;
-    if (is_2MediumPropertie) {
-        var municipie = state.find(x => x.slug === this.value)
+    } else if (this.value == "2_mediumPropertie") {
         document.querySelector('input[id=module]').value = municipie.num;
         document.querySelector('p[id=propertieClassification]').innerHTML = 'Média propriedade';
         document.querySelector('p[id=app_margem_corrego]').innerHTML = '30 metros';
         document.querySelector('p[id=app_nascente]').innerHTML = '15 metros';
         document.querySelector('p[id=reseva_legal]').innerHTML = '20%, pode contar a APP';
-    }
-})
 
-
-//onChange if it's isBigPropertie
-document.querySelector('select[id=municipie]').addEventListener('change', function() {
-    const state = data[selectState.options[selectState.selectedIndex].value]
-    let current = document.querySelector('select[id=fiscalModule]');
-    let isBigPropertie = current.options[current.selectedIndex].value == 'bigPropertie' ? true : false;
-    var municipie = state.find(x => x.slug === this.value)
-    if (isBigPropertie) {
+    } else if (this.value == "bigPropertie") {
         document.querySelector('input[id=module]').value = municipie.num;
         document.querySelector('p[id=propertieClassification]').innerHTML = 'grande latifúndio';
         document.querySelector('p[id=app_margem_corrego]').innerHTML = '30 metros';
