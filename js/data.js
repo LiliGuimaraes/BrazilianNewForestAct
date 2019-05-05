f23cd36273adf86c7fea223ac1319f1dcf4a93f6
Lilian Guimarães <lilian.guimaraes.silva@gmail.com>
2019-05-04 23:06:10 -0300

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,22 +1,28 @@
 const data = {
     "MG": [{ "slug": "belo-horizonte", "nome": "Belo Horizonte", "num": 20 }],
-    "SP": [{ "slug": "sao-paulo", "nome": "São Paulo", "num": 20 }]
+    "SP": [{ "slug": "sao-paulo", "nome": "São Paulo", "num": 40 }]
 }
-var selectState = document.querySelector('select[id=country');
+var selectState = document.querySelector('select[id=state');
 // populate select with data structure
-for (key in data) {
+selectState.options[selectState.options.length] = new Option('---', '---')
+for (var key in data) {
     selectState.options[selectState.options.length] = new Option(key, key)
 }
 
-var selectMunicipies = document.querySelector('select[id=state]');
-var selectInputModule = document.querySelector('input[id=module]')
-
 // populate select with data structure
+let selectMunicipies = document.querySelector('select[id=municipie]');
+selectMunicipies.options[selectMunicipies.options.length] = new Option('---', '---')
 for (var key of Object.keys(data)) {
-    console.log("key", data[key])
     for (var city of data[key]) {
-        console.log("city", city)
         selectMunicipies.options[selectMunicipies.options.length] = new Option(city.nome, city.slug)
-        $('#module').text('O valor do módulo fiscal da sua região é de ' + city.num);
     }
 }
+
+document.querySelector('select[id=municipie]').addEventListener('change', function() {
+        const state = data[selectState.options[selectState.selectedIndex].value]
+        console.log(this.value);
+        console.log('state', state);
+        var municipie = state.find(x => x.slug === this.value)
+        console.log('find', find)
+        document.querySelector('input[id=module]').value = municipie.num;
+    })
